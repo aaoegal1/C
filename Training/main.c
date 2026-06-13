d617ca3c463eb356537bb2a7f37bdb16262f9ecd
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 09:15:53 +0300

    main.c - \n

---
+++ Training/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Standard input output
+int main() { //main is starting point of out program also is Function
+    printf("Hello Abdirizak \n");
+    printf("Da'dayda waaa 21 jir");  // the result will be one line so to make 2 line output use \n to seperate the lines
+    return 0;
+
+}
