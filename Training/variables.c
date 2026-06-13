3c0d2f8af2569ece31fb5f70f6a6550c8e61884c
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 10:23:06 +0300

    Variables - Done - Expalantion

--- Training/variables.c
+++ Training/variables.c
@@ -9,56 +9,56 @@ int main() {
     // Variable = a reusable container for a value.
     //            u dhaqma waxa ku jira isla box container.
 
-    // int age = 21;
-    // int year = 2004;
-    // int rg_year = 2026;
+    int age = 21;
+    int year = 2004;
+    int rg_year = 2026;
 
-    // printf("My age is %d years old.\n" , age);
-    // printf("The year i born is %d. \n", year);
-    // printf("The current year where right now is %d. \n", rg_year);
-
-
-
-    // float gpa = 3.92;
-    // float price = 7.99;
-    // float tem = -10.1;
-    //
-    //
-    // printf("my GPA is %.2f\n", gpa);  // the result will be '3.920000' so we don't need that to get the exactly we need to clarify how many points we need so this time use %.2
-    // printf("I used to pay my Netlfix acc $%.2f price per month.\n", price);
-    // printf("the temperature is %.1f\n", tem);
-
-
-    // double pi = 3.14159265359;
-    // double distance = 9.455535433;
-    //
-    //
-    // printf("the value of pi is %.11lf\n", pi);
-    // printf("the distance home to uni is %.9lf\n", distance);
-
-
-    // char grade = 'A';
-    // char symbol = '!';
-    // char symbol1 = '=';
-    // char currency = '$';
-    //
-    // printf("my grade is %c+\n", grade);
-    // printf("my fav symbol is %c\n", symbol);
-    // printf("the 2+2 is %c 4\n", symbol1);
-    // printf("the US currency is %c dollor sign\n", currency);
+    printf("My age is %d years old.\n" , age);
+    printf("The year i born is %d. \n", year);
+    printf("The current year where right now is %d. \n", rg_year);
 
 
 
-    // char name[] = "Abdirizak Adam Osman";
-    // char uni[] = "Puntland State University";
-    // char city[] = "Galkio";
-    // char email[] = "aaoegal@gmail.com";
-    //
-    //
-    // printf("My full name is %s\n", name);
-    // printf("My university name is %s\n", uni);
-    // printf("The city i born is %s\n", city);
-    // printf("my email is %s\n", email);
+    float gpa = 3.92;
+    float price = 7.99;
+    float tem = -10.1;
+
+
+    printf("my GPA is %.2f\n", gpa);  // the result will be '3.920000' so we don't need that to get the exactly we need to clarify how many points we need so this time use %.2
+    printf("I used to pay my Netlfix acc $%.2f price per month.\n", price);
+    printf("the temperature is %.1f\n", tem);
+
+
+    double pi = 3.14159265359;
+    double distance = 9.455535433;
+
+
+    printf("the value of pi is %.11lf\n", pi);
+    printf("the distance home to uni is %.9lf\n", distance);
+
+
+    char grade = 'A';
+    char symbol = '!';
+    char symbol1 = '=';
+    char currency = '$';
+
+    printf("my grade is %c+\n", grade);
+    printf("my fav symbol is %c\n", symbol);
+    printf("the 2+2 is %c 4\n", symbol1);
+    printf("the US currency is %c dollor sign\n", currency);
+
+
+
+    char name[] = "Abdirizak Adam Osman";
+    char uni[] = "Puntland State University";
+    char city[] = "Galkio";
+    char email[] = "aaoegal@gmail.com";
+
+
+    printf("My full name is %s\n", name);
+    printf("My university name is %s\n", uni);
+    printf("The city i born is %s\n", city);
+    printf("my email is %s\n", email);
 
 
     // Boolean we need new header file called stdbool.h
@@ -78,6 +78,13 @@ int main() {
 
 
 
+// ------------------------------------------------------------------------------------------------------------
+    //int = whole numbers  - 4 byte
+    // float = single-precision decimal number -  4 byte
+    // double = double-precision decimal number - 8 byte
+    // char = single character - 1 byte
+    // char[] = array of characters - size varies
+    // bool - true of false - 1 byte  requires new <stdbool.h>
 
 
     return 0;
