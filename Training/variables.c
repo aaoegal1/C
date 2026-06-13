317f755e9e004dd51f13d12ecf82fbaffd752d82
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 10:17:52 +0300

    Variables - Boolean

--- Training/variables.c
+++ Training/variables.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 int main() {
 
     // Variable = a reusable container for a value.
@@ -48,16 +49,32 @@ int main() {
 
 
 
-    char name[] = "Abdirizak Adam Osman";
-    char uni[] = "Puntland State University";
-    char city[] = "Galkio";
-    char email[] = "aaoegal@gmail.com";
+    // char name[] = "Abdirizak Adam Osman";
+    // char uni[] = "Puntland State University";
+    // char city[] = "Galkio";
+    // char email[] = "aaoegal@gmail.com";
+    //
+    //
+    // printf("My full name is %s\n", name);
+    // printf("My university name is %s\n", uni);
+    // printf("The city i born is %s\n", city);
+    // printf("my email is %s\n", email);
 
 
-    printf("My full name is %s\n", name);
-    printf("My university name is %s\n", uni);
-    printf("The city i born is %s\n", city);
-    printf("my email is %s\n", email);
+    // Boolean we need new header file called stdbool.h
+
+    bool isTall = true;
+    bool isWeird = false;
+
+    printf("%d\n", isTall);
+    printf("%d\n", isWeird);
+
+    if (isWeird) {
+        printf( "weirdo");
+    }
+        else {
+            printf("he's cool");
+    }
 
 
 
