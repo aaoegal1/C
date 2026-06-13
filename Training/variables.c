e06edd977b137fbb3274841135f3271fb8543362
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 09:47:32 +0300

    Variables - Float

--- Training/variables.c
+++ Training/variables.c
@@ -8,14 +8,24 @@ int main() {
     // Variable = a reusable container for a value.
     //            u dhaqma waxa ku jira isla box container.
 
-    int age = 21;
-    int year = 2004;
-    int rg_year = 2026;
+    // int age = 21;
+    // int year = 2004;
+    // int rg_year = 2026;
 
-    printf("My age is %d years old.\n" , age);
-    printf("The year i born is %d. \n", year);
-    printf("The current year where right now is %d. \n", rg_year);
+    // printf("My age is %d years old.\n" , age);
+    // printf("The year i born is %d. \n", year);
+    // printf("The current year where right now is %d. \n", rg_year);
 
+
+
+    float gpa = 3.92;
+    float price = 7.99;
+    float tem = -10.1;
+
+
+    printf("my GPA is %.2f\n", gpa);  // the result will be '3.920000' so we don't need that to get the exactly we need to clarify how many points we need so this time use %.2
+    printf("i used to pay my Netlfix acc $%.2f price per month.\n", price);
+    printf("the temperature is %.1f\n", tem);
 
 
     return 0;
