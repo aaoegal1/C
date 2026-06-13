e74979b93b604181d5bfffff32cb7954c0070cd0
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 09:26:10 +0300

    Variables - Int

---
+++ Training/variables.c
@@ -0,0 +1,22 @@
+//
+// Created by Abdirizak on 6/13/2026.
+//
+
+#include <stdio.h>
+int main() {
+
+    // Variable = a reusable container for a value.
+    //            u dhaqma waxa ku jira isla box container.
+
+    int age = 21;
+    int year = 2004;
+    int rg_year = 2026;
+
+    printf("My age is %d years old.\n" , age);
+    printf("The year i born is %d. \n", year);
+    printf("The current year where right now is %d. \n", rg_year);
+
+
+
+    return 0;
+}
