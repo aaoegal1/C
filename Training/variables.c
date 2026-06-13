abd0ec8c2c6d0dc5b76e29c131854d0128f3fe13
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-13 09:54:46 +0300

    Variables - Double

--- Training/variables.c
+++ Training/variables.c
@@ -18,14 +18,23 @@ int main() {
 
 
 
-    float gpa = 3.92;
-    float price = 7.99;
-    float tem = -10.1;
+    // float gpa = 3.92;
+    // float price = 7.99;
+    // float tem = -10.1;
+    //
+    //
+    // printf("my GPA is %.2f\n", gpa);  // the result will be '3.920000' so we don't need that to get the exactly we need to clarify how many points we need so this time use %.2
+    // printf("i used to pay my Netlfix acc $%.2f price per month.\n", price);
+    // printf("the temperature is %.1f\n", tem);
 
 
-    printf("my GPA is %.2f\n", gpa);  // the result will be '3.920000' so we don't need that to get the exactly we need to clarify how many points we need so this time use %.2
-    printf("i used to pay my Netlfix acc $%.2f price per month.\n", price);
-    printf("the temperature is %.1f\n", tem);
+    double pi = 3.14159265359;
+    double distance = 9.455535433;
+
+
+    printf("the value of pi is %.11lf\n", pi);
+    printf("the distance home to uni is %.9lf\n", distance);
+
 
 
     return 0;
