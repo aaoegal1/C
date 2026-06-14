87238ce65399502dd6f008aa3dd7e7bc437d06c1
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-14 07:36:41 +0300

    Format Specifier

---
+++ Training/memory.c
@@ -0,0 +1,38 @@
+//
+// Created by Abdirizak on 6/14/2026.
+//
+
+#include <stdio.h>
+int main() {
+
+    //
+    int x = 5;
+    int a = 10;
+    int b = 22;
+    int c = 229;
+    int d = 2004;
+    int e = 0214;
+    int f = 1234;
+
+
+
+    printf("%d\n", x);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    printf("%d\n", e);
+    printf("%d\n", f);
+
+    // address-ka waxaa lagu helaa: &variable name
+
+    printf("%p\n", &x);
+    printf("%p\n", &a);
+    printf("%p\n", &b);
+    printf("%p\n", &c);
+    printf("%p\n", &d);
+    printf("%p\n", &e);
+    printf("%p\n", &f);
+
+    return 0;
+}
