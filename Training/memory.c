fbb5edba2dfceb787f4c83407efc9aa8a0216586
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-14 07:50:51 +0300

    Memory - &x - *p

--- Training/memory.c
+++ Training/memory.c
@@ -5,34 +5,48 @@
 #include <stdio.h>
 int main() {
 
+    // //
+    // int x = 5;
+    // int a = 10;
+    // int b = 22;
+    // int c = 229;
+    // int d = 2004;
+    // int e = 0214;
+    // int f = 1234;
     //
-    int x = 5;
-    int a = 10;
-    int b = 22;
-    int c = 229;
-    int d = 2004;
-    int e = 0214;
-    int f = 1234;
+    //
+    //
+    // printf("%d\n", x);
+    // printf("%d\n", a);
+    // printf("%d\n", b);
+    // printf("%d\n", c);
+    // printf("%d\n", d);
+    // printf("%d\n", e);
+    // printf("%d\n", f);
+    //
+    // // address-ka waxaa lagu helaa: &variable name
+    //
+    // printf("%p\n", &x);
+    // printf("%p\n", &a);
+    // printf("%p\n", &b);
+    // printf("%p\n", &c);
+    // printf("%p\n", &d);
+    // printf("%p\n", &e);
+    // printf("%p\n", &f);
 
 
 
-    printf("%d\n", x);
-    printf("%d\n", a);
-    printf("%d\n", b);
-    printf("%d\n", c);
-    printf("%d\n", d);
-    printf("%d\n", e);
-    printf("%d\n", f);
 
-    // address-ka waxaa lagu helaa: &variable name
 
-    printf("%p\n", &x);
-    printf("%p\n", &a);
-    printf("%p\n", &b);
-    printf("%p\n", &c); c
-    printf("%p\n", &d);
-    printf("%p\n", &e);
-    printf("%p\n", &f);
-
-    return 0;
+     // x   -> 5 - value = 5
+    //  p   -> address of x - address = 000000f89c1ffcc4
+   //  &p  -> address of p - 0000000000000005
+    // int x = 5;
+    // int *p = &x;
+    // printf("value = %d\n", x);
+    // printf("address = %p\n", &x);
+    // printf("Pointer = %p\n", *p);
+    // printf("%p\n", p);
+    //
+    // return 0;
 }
