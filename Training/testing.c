ba3af2e42417ee37f72817943be52cb4133d977e
Abdirizak Adam Osman <aaoegal@gmail.com>
2026-06-20 16:57:12 +0300

    testing

---
+++ Training/testing.c
@@ -0,0 +1,26 @@
+//
+// Created by Abdirizak on 6/20/2026.
+//
+
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    int *p = &x;  // so saar value X - *p wuxuu hayaa addresska X
+
+    printf("x = %d\n", x);
+    printf("*p = %d\n", *p);
+
+    *p = 50; // badal qiimaha addresska X oo ahaa 5 u badal 50 so hada ouput X = 50
+
+    printf("x = %d\n", x);
+    printf("*p = %d\n", *p);
+
+    p = &y;  // u wareeg pointer laguu tilmaamay oo ah y
+
+    printf("*p = %d\n", *p); 
+
+    return 0;
+}
